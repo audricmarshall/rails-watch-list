5fb7475284a6fd3874937baf59a751afeb2e066e
Audric Nomentsoa <audricmarshall@gmail.com>
2025-06-17 22:53:33 +0200

    ajout de toutes les routes lists/bookmarks avec nesting, root sur index pour éviter la page rails sur localholst:3000

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "list#index" # on root direct vers la page index pour éviter la page rails
+  # je declare les routes avec resources pour les listes
+  resources :lists, only: %i[index show new create] do
+    # je nest les routes bookmarks dans une liste
+    resources :bookmarks, only: %i[new create]
+  end
+  # je met la root destroy en dehors pour supprimer un bookmark, pas besoin de
+  # préciser dans quel liste il est car chaque bookmarks a un ID unique
+  resources :bookmarks, only: %i[destroy]
 end
