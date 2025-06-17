5fe65688244d7fe8b6b8a54dc4273b158f6a8cb6
Audric Nomentsoa <audricmarshall@gmail.com>
2025-06-17 22:27:41 +0200

    ajoute la seed de 4 films et vérifie la création avec rails db:seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_17_200547) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_17_201607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_17_200547) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id", "list_id"], name: "index_bookmarks_on_movie_id_and_list_id", unique: true
     t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
